32df948fa3cd5ff58c4d0b54801975b924724f0b
i-bm <isaac.boakyemanu@gmail.com>
2021-03-16 14:56:41 +0000

    Updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,6 @@
 'use strict';
 
-
-
 const myTime = () => {
-    // const currentDateTime = new Date();
-    // const myTimeZone = currentDateTime.getTimezoneOffset();
     setInterval(function(){
         document.querySelector('#time').textContent = moment().format("DD/MM/YYYY HH:mm:ss")+ ` UTC +0 ` ;
     }, 1000);
